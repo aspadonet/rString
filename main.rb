655fc87e89e4d725ace48b01682ec67441eed88b
aleksandr aleksandr <44408867+aspadonet@users.noreply.github.com>
2022-05-27 09:31:22 +0000

    one

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
-puts "Hello, World!"
+#puts "Hello, World!"
+
+fName = "one"
+lName = "two"
+puts fName + " " + lName
+fullName = fName + " " + lName
+puts "my first name is #{fName} and #{lName}"
+puts fName.class
+puts fName.length
+puts fName.reverse
+puts fName.capitalize
+puts fName.empty?
+puts fullName.sub("two","thrie")
+puts fullName.methods
