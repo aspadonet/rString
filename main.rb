842169d9f0c76ca40d265b8bd0b5e7243b2be625
aleksandr aleksandr <44408867+aspadonet@users.noreply.github.com>
2022-05-28 17:59:50 +0000

    array

--- main.rb
+++ main.rb
@@ -12,3 +12,12 @@ puts fName.capitalize
 puts fName.empty?
 puts fullName.sub("two","thrie")
 puts fullName.methods
+newFName = fName
+fName = "John"
+puts newFName
+puts fName
+puts tSting = "the new first name is #{newFName} \#{newFName}"
+puts "what is your first name?"
+myFirstName = gets.chop
+puts myFirstName 
+puts 7 + 8
